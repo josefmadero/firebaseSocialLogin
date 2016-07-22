8ee4e6f74fa0e35e1b4db7fd95e498e50b5a84f2
lrdm <luisrdm@gmail.com>
2016-07-22 13:51:50 -0300

    ...

---
+++ app/src/main/java/com/luisrdm/firebaseandsocialloginentregable/dao/PaintingDao.java
@@ -0,0 +1,17 @@
+package com.luisrdm.firebaseandsocialloginentregable.dao;
+
+import com.luisrdm.firebaseandsocialloginentregable.model.Painting;
+
+/**
+ * Created by izu on 21/07/2016.
+ */
+public class PaintingDao {
+
+    public Painting getPaintingFromFirebase (String paintingName){
+
+
+
+        return null;
+    }
+
+}
